eacfdea17daebca185dc038a1900978954d42af0
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-07-04 18:22:12 +0700

    Update init.h

--- SPWS/init.h
+++ SPWS/init.h
@@ -1,22 +1,58 @@
+#ifndef INIT_H
+#define INIT_H
+
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+uint8_t     mode_button_pressed     = 0;
+uint8_t     pump_button_pressed     = 0; 
+uint8_t     temperature;
+uint8_t     moisture;
 
 
-#define MAX_HUMID 80
-#define MIN_HUMID 60
-#define MODE_MANUAL 0
-#define MODE_AUTO 1
-#define PUMP_OFF 0
-#define PUMP_ON 1
-#define LED_NORMAL 7
-#define LED_WATERING 8
-#define LED_LOW_MOISTURE_ALERT 9
-#define LED_ERROR 10
+typedef enum
+{
+    MODE_AUTO   = 1,
+    MODE_MANUAL = 0 
+} system_mode;
+system_mode sys_mode;
+
+typedef enum
+{
+    PUMP_ON     = 1,
+    PUMP_OFF    = 0
+} pump_state;
+
+typedef enum
+{
+    SYS_NORMAL                  = 7,
+    SYS_WATERING                = 8,
+    SYS_LOW_MOISTURE_ALERT      = 9,
+    SYS_ERROR                   = 10
+} system_state;
+
+typedef struct
+{
+    uint8_t max_moisture_threshold;
+    uint8_t min_moisture_threshold;
+    uint8_t pump_time;
+    uint8_t sys_delay;
+    system_mode sys_mode;
+    pump_state pump;
+} config_t;
+config_t system_config;
+
+void init_default_config()
+{
+    system_config.max_moisture_threshold = 80;
+    system_config.min_moisture_threshold = 50;
+    system_config.pump_time    = 10;
+    system_config.sys_delay    = 5;
+    system_config.sys_mode  = MODE_AUTO;
+    system_config.pump = PUMP_OFF;
+}
 
 
-uint8_t sys_mode;
-uint8_t mode_button_pressed = 0;
-uint8_t pump_button_pressed = 0; 
-uint16_t temperature;
-uint16_t humidity;
-uint32_t led_state;
+#endif
