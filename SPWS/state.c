55e42991b4755cf07680b44f3d976fc3b81d8cd5
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-07-04 23:54:52 +0700

    Update state.c

--- SPWS/state.c
+++ SPWS/state.c
@@ -3,7 +3,7 @@
 int turn_pump_off();
 int turn_pump_on();
 
-int system_update() //for a whole system
+int system_update()
 {
     system_state systemState; 
     if((turn_pump_off()) && moisture >= system_config.min_moisture_threshold)
