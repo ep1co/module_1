691b7826e033558bb288de42907753619a46c0cb
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-07-05 01:03:39 +0700

    Update adc.c

--- SPWS/adc.c
+++ SPWS/adc.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <init.h>
-#include <stdint.h>
+#include "init.h"
+
+extern uint8_t temperature;
+extern uint8_t moisture;
 
 void update_temperature()
 {
     temperature = rand() % (30 - 25 + 1) + 25;
 }
 
-void update_humidity()
+void update_moisture()
 {
-    humidity = rand() % (90 - 20 + 1) + 20;
+    moisture = rand() % (90 - 20 + 1) + 20;
 }
