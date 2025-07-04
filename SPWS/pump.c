b370473325fa9bc4318063b3cfa546eb0d5a5cc2
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-07-04 18:25:35 +0700

    Update pump.c

--- SPWS/pump.c
+++ SPWS/pump.c
@@ -1,29 +1,59 @@
-#include <stdio.h>
-#include <init.h>
+#include "init.h"
 
-extern int temperature;
-extern int humidity;
+extern uint8_t moisture;
+
+int system_update();
 
 void turn_pump_on()
 {
-
+    if(system_config.pump == PUMP_OFF)
+    {
+        system_config.pump = PUMP_ON; 
+    }
+    sleep(system_config.pump_time);
 }
 
 void turn_pump_off()
 {
-    
+    if(system_config.pump == PUMP_ON)
+    {
+        system_config.pump = PUMP_OFF; 
+    }
 }
 
-struct state
+void auto_pump()
 {
-
-};
-
-void pump_state()
-{
-    if (humidity >= MAX_HUMID)
+    switch ((system_update()))
     {
-        turn_pump_off();
+        case SYS_NORMAL:
+            printf("humidity is ideal, no need to water\n");
+            turn_pump_off();
+            break;
+        
+        case SYS_WATERING:
+            printf("watering\n");
+            sleep(system_config.pump_time);
+            while(sleep)
+            {
+                for(int i = 0; i < system_config.pump_time; i++)
+                {
+                    moisture++;
+                    system_update();
+                    auto_pump();
+                    sleep(1);
+                }
+            }
+            break;
+        
+        case SYS_LOW_MOISTURE_ALERT:
+            printf("YELLOW\n");
+            break;
+        
+        case SYS_ERROR:
+            printf("RED\n");
+            break;
+
+        default:
+            break;
     }
-    else turn_pump_on();
 }
