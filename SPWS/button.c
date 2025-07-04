9e9e254538d2a95a22a65279c4650e09ed07ec0c
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-07-04 23:47:57 +0700

    Update button.c

--- SPWS/button.c
+++ SPWS/button.c
@@ -1,6 +1,17 @@
+/*
+co 2 loai nut: nut chuyen mode va nut tuoi
+mac dinh la mode AUTO
+an nut 1 -> doi mode thanh MANUAL
+an nut 2 -> tuoi nuoc
+*/
+
 #include "init.h"
 
-void init_button()
+void auto_pump();
+void turn_pump_off();
+
+
+void init_mode_button()
 {
     if (mode_button_pressed) 
     {
@@ -9,15 +20,20 @@ void init_button()
             turn_pump_off();
             system_config.sys_mode = MODE_MANUAL;
             printf("Manual mode enabled.\n");
+            printf("Press 'W' to water.\n");
+            press_button();
         }
         else
         {
             system_config.sys_mode = MODE_AUTO;
             printf("Auto mode enabled.\n");
-            printf("Press any key to water.\n");
+            auto_pump();
         }
     }
+}
 
+void init_pump_button()
+{
     if (pump_button_pressed)
     {
         if (system_config.pump == PUMP_OFF)
@@ -33,3 +49,16 @@ void init_button()
         }
     }
 }
+    
+void press_button()
+{
+    char ch = getchar();
+    while (ch != '\n')
+    {
+        if (ch == 'w') system_config.pump = PUMP_ON;
+        else if (ch == 'p') init_mode_button();
+        else if (ch == 'm') init_pump_button();
+    }
+}
+
+    
