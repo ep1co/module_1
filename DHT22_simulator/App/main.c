f10a9f6256187853812eb14b9c44ebfa817da4c1
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-09-12 22:52:51 +0700

    Update main.c

--- DHT22_simulator/App/main.c
+++ DHT22_simulator/App/main.c
@@ -1,50 +1,52 @@
-// include header files
-//#include "DHT22.h"
 #include "Test_Service.h"
 #include "DHT22.h"
 
 
-
-// local constants
-#define name_size               (30)
-
-
-//local data type
-
-
-
-// local variables
-
-
-
-// global function prototype
 void Create_Data_Test();
 bool DHT_Init();
 bool DHT_Read_Temperature();
 bool DHT_Read_Humidity();
 
 
-// global function definition
 int main(void)
 {
     float temp, humi;
     printf("Insert desired temperature and humid, separated by space: ");
-    scanf("%f %f", &temp, &humi);
+    if (scanf("%f %f", &temp, &humi) != 2)
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
     printf("Type file name to save data: ");
     char fileName[name_size];
     scanf("%29s", fileName);
     Create_Data_Test(fileName, temp, humi);
-    DHT_Init(fileName);
-    if(DHT_Read_Temperature(&temp))
+    if (DHT_Init(fileName))
     {
-        printf("Temperature: %.1f\n", temp);
+        if (DHT_Read_Temperature(&temp))
+        {
+            printf("Temperature: %.1f\n", temp);
+        }
+        else
+        {
+            printf("Failed to read temperature\n");
+        }
+        if (DHT_Read_Humidity(&humi))
+        {
+            printf("Humidity: %.1f\n", humi);
+        }
+        else
+        {
+            printf("Failed to read humidity\n");
+        }
+        if (fd_r != NULL)
+        {
+            fclose(fd_r);
+            fd_r = NULL;
+        }
     }
-    if(DHT_Read_Humidity(&humi))
-    {
-        printf("Humidity: %.1f\n", humi);
-    }
-
     return 0;
 }
+
 
 
