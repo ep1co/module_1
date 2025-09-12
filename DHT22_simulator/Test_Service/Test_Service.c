0a69ebf60f857fde24f1b7c69b26454834d4b523
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-09-12 22:35:34 +0700

    Update Test_Service.c

--- DHT22_simulator/Test_Service/Test_Service.c
+++ DHT22_simulator/Test_Service/Test_Service.c
@@ -20,46 +20,67 @@ generate 5 byte: 2 byte temp, 2 for humid, 1 for checksum
 */
 void Create_Data_Test(const char *filename, float temp, float humidity)
 {
-    int fd = open(filename, O_RDWR | O_CREAT, 0644);
-    if(fd < 0)
+    int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0)
     {
         perror("Can't open file");
         exit(1);
     }
-    printf("File created successfully. Ready to write data.\n");
-    // Product with 10 then convert to binary 16bit
-    uint16_t temp_bin  = (uint16_t)(temp * 10);
+
+    // check overflow
+    if (temp * 10 > UINT16_MAX || humidity * 10 > UINT16_MAX)
+    {
+        fprintf(stderr, "Temperature or humidity too large\n");
+        close(fd);
+        exit(1);
+    }
+
+    // float to binary
+    uint16_t temp_bin = (uint16_t)(temp * 10);
     uint16_t humid_bin = (uint16_t)(humidity * 10);
-
-    // Caculate checksum
-    uint8_t temp_high  = (temp_bin >> 8) & 0xFF;
-    uint8_t temp_low   = temp_bin & 0xFF;
+    
+    uint8_t temp_high = (temp_bin >> 8) & 0xFF;
+    uint8_t temp_low = temp_bin & 0xFF;
     uint8_t humid_high = (humid_bin >> 8) & 0xFF;
-    uint8_t humid_low  = humid_bin & 0xFF;
-
+    uint8_t humid_low = humid_bin & 0xFF;
     uint8_t checksum = temp_high + temp_low + humid_high + humid_low;
 
-    printf("Inserting start bits...\n");
-
-    // 4. Ghi từng bit của temp (16 bit)
-    for (int i = (temp_data_length - 1); i >= 0; i--) {
+    for (int i = temp_size - 1; i >= 0; i--)
+    {
         uint8_t bit = (temp_bin >> i) & 1;
-        dprintf(fd, "%d\n", bit);
+        if (dprintf(fd, "%d\n", bit) < 0)
+        {
+            perror("Failed to write temperature bit");
+            close(fd);
+            exit(1);
+        }
     }
 
-    // 5. Ghi từng bit của humid (16 bit)
-    for (int i = (humid_data_length - 1); i >= 0; i--) {
+    for (int i = humi_size - 1; i >= 0; i--)
+    {
         uint8_t bit = (humid_bin >> i) & 1;
-        dprintf(fd, "%d\n", bit);
+        if (dprintf(fd, "%d\n", bit) < 0)
+        {
+            perror("Failed to write humidity bit");
+            close(fd);
+            exit(1);
+        }
     }
 
-    // 6. Ghi từng bit của checksum (8 bit)
-    for (int i = (checksum_data_length - 1); i >= 0; i--) {
+    for (int i = checksum_length - 1; i >= 0; i--)
+    {
         uint8_t bit = (checksum >> i) & 1;
-        dprintf(fd, "%d\n", bit);
+        if (dprintf(fd, "%d\n", bit) < 0)
+        {
+            perror("Failed to write checksum bit");
+            close(fd);
+            exit(1);
+        }
     }
-    printf("Data written successfully. Closing file.\n");
+
     close(fd);
+    printf("Data written to %s successfully.\n", filename);
 }
+
 
 
